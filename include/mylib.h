81be059b5c43e43392ae5d7bcdf6b2a499d51f9b
Will Lee <step.far.away@gmail.com>
2023-05-13 20:37:14 +0800

    cmake library test

---
+++ include/mylib.h
@@ -0,0 +1,18 @@
+//
+// Created by Will Lee on 2023/5/13.
+//
+
+#ifndef CMAKE_LIBRARY__MYLIB_H_
+#define CMAKE_LIBRARY__MYLIB_H_
+
+namespace mylib {
+
+// A function that adds two integers
+int add(int a, int b);
+
+// A function that subtracts two integers
+int sub(int a, int b);
+
+}
+
+#endif //CMAKE_LIBRARY__MYLIB_H_
---
+++ src/mylib.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Will Lee on 2023/5/13.
+//
+
+#include "../include/mylib.h"
+
+int mylib::add(int a, int b) {
+  return a + b;
+}
+
+int mylib::sub(int a, int b) {
+  return a - b;
+}
